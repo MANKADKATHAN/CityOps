53a083222004c59377e7da090138a21e05ee0449
MANKADKATHAN <kathanmankad123@gmail.com>
2026-01-28 22:56:32 +0530

    Remove sensitive SQL migration files

--- civicpulse-backend/add_foreign_key.sql
+++
@@ -1,6 +0,0 @@
--- Create the foreign key relationship to allow joining on user_id
-alter table complaints
-add constraint fk_complaints_user
-foreign key (user_id)
-references profiles (id)
-on delete set null;
